06340df0fcba4107717cecf835f2051f2b57bcea
Anne <anne.kreischer@gmail.com>
2025-04-02 16:53:33 +0200

    app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '/about' }
+];
